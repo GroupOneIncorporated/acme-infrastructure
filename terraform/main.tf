e22bea0a8308affcf7289bc88cd22cc30c1f4c3f
Jose Barroca <josebarroca@gmail.com>
2020-10-08 10:35:17 +0200

    testing creating instance, ERROR, must create network first?

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,17 @@ resource "openstack_compute_keypair_v2" "my-cloud-key" {
   # Doc: https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_keypair_v2#import
   name = "jd222qf_Keypair"
 }
+
+resource "openstack_compute_instance_v2" "test" {
+  name            = "test-vm"
+  image_name      = "Ubuntu Minimal 18.04"
+  flavor_name     = "c1-r1-d10"
+  # Using the keypair resource previously created
+  key_pair        = openstack_compute_keypair_v2.my-cloud-key.name
+  security_groups = ["default"]
+  availability_zone_hints = "Education"
+
+  network {
+    name = "public"
+  }
+}
