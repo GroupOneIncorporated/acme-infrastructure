41d2011e81fc7f3b9c2aa80a4bd10bf4741a44c2
Jose Barroca <josebarroca@gmail.com>
2020-10-09 10:46:49 +0200

    added and allocated floating ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,7 @@ resource "openstack_compute_instance_v2" "test" {
   name            = "test-vm"
   image_name      = "Ubuntu Minimal 18.04"
   flavor_name     = "c1-r1-d10"
+  //count = ...
   # Using the keypair resource previously created
   key_pair        = openstack_compute_keypair_v2.my-cloud-key.name
   security_groups = ["default"]
@@ -43,3 +44,25 @@ resource "openstack_compute_instance_v2" "test" {
     name = "internal-network"
   }
 }
+
+
+// ---- FLOATING IPs ----
+// Required for external access to instances
+// Openstack does not automatically assign floating IPs, they are allocated by administrators.
+// Project has 3 available floating IPs (pool = "public") per user
+// resources: network_floatingip_v2 to create (compute_floatingip is deprecated), compute_floatingip_associate_v2 to assign to compute instances
+
+resource "openstack_networking_floatingip_v2" "float-ip" {
+  //count = ...
+  pool = "public" //required. project pool has 3 floating ips available
+}
+
+resource "openstack_compute_floatingip_associate_v2" "float-ip-associate" {
+  //count = ...
+  instance_id = openstack_compute_instance_v2.test.id
+  floating_ip = openstack_networking_floatingip_v2.float-ip.address
+}
+
+
+
+
