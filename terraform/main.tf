79eb43f3e0ab8ec7dbab6a89226ba15d0da73ee3
Jose Barroca <josebarroca@gmail.com>
2020-10-09 10:51:17 +0200

    fixing comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,27 @@
+// ---------- KEY PAIR ----------
 resource "openstack_compute_keypair_v2" "my-cloud-key" {
   # Keypair on Openstack imported into the state file using "terraform import"
   # Doc: https://registry.terraform.io/providers/terraform-provider-openstack/openstack/latest/docs/resources/compute_keypair_v2#import
   name = "jd222qf_Keypair"
 }
 
-// creating internal network and subnet
+
+// ---------- NETWORK ----------
+// Creating internal network and respective subnet
+// Subnet is not mandatory per se, but OpenStack requires one for attaching instances to a network
+// Routing is needed to provide external network access to servers on the internal network
+// Routing interface is needed to connect both networks
 resource "openstack_networking_network_v2" "internal-network" {
   name           = "internal-network"
   admin_state_up = "true"
 }
 
-// (subnet is required for attaching instances to network)
 resource "openstack_networking_subnet_v2" "internal-subnet" {
   name       = "internal-subnet"
   network_id = openstack_networking_network_v2.internal-network.id
   cidr       = "192.168.199.0/24"
 }
 
-// adding router (to provide external network access for servers on internal network) and its interface (to connect the networks)
 resource "openstack_networking_router_v2" "router-1" {
   name = "router-1"
   admin_state_up = true
@@ -30,6 +34,7 @@ resource "openstack_networking_router_interface_v2" "router-1-interface" {
 }
 
 
+// ---------- INSTANCES ----------
 resource "openstack_compute_instance_v2" "test" {
   name            = "test-vm"
   image_name      = "Ubuntu Minimal 18.04"
@@ -46,7 +51,7 @@ resource "openstack_compute_instance_v2" "test" {
 }
 
 
-// ---- FLOATING IPs ----
+// ---------- FLOATING IPs ----------
 // Required for external access to instances
 // Openstack does not automatically assign floating IPs, they are allocated by administrators.
 // Project has 3 available floating IPs (pool = "public") per user
