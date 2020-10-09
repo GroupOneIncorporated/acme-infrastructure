b903a88f2cdaf7348662c69d375ac77a07b7da2c
Jose Barroca <josebarroca@gmail.com>
2020-10-09 09:50:13 +0200

    added router and router interface

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,16 +4,31 @@ resource "openstack_compute_keypair_v2" "my-cloud-key" {
   name = "jd222qf_Keypair"
 }
 
+// creating internal network and subnet
 resource "openstack_networking_network_v2" "internal-network" {
   name           = "internal-network"
   admin_state_up = "true"
 }
 
+// (subnet is required for attaching instances to network)
 resource "openstack_networking_subnet_v2" "internal-subnet" {
   name       = "internal-subnet"
   network_id = openstack_networking_network_v2.internal-network.id
   cidr       = "192.168.199.0/24"
 }
+
+// adding router (to provide external network access for servers on internal network) and its interface (to connect the networks)
+resource "openstack_networking_router_v2" "router-1" {
+  name = "router-1"
+  admin_state_up = true
+  external_network_id = "fd401e50-9484-4883-9672-a2814089528c" //from OpenStack dashboard
+}
+
+resource "openstack_networking_router_interface_v2" "router-1-interface" {
+  router_id = openstack_networking_router_v2.router-1.id
+  subnet_id = openstack_networking_subnet_v2.internal-subnet.id
+}
+
 
 resource "openstack_compute_instance_v2" "test" {
   name            = "test-vm"
