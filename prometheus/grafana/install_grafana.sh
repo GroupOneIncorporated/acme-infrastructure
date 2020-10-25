12a37379b0ddd95a17f61f285207a4b5af14825f
scoobis <gabriel.bjorlin@gmail.com>
2020-10-25 23:36:36 +0100

    bin/bash and readability

--- prometheus/grafana/install_grafana.sh
+++ prometheus/grafana/install_grafana.sh
@@ -1,10 +1,13 @@
-echo "Get Grafana"
+#!/bin/bash
+# Installing Grafana on Debian
+
+echo "******************************* Get Grafana *******************************"
 wget -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 
-echo "Update and install grafana"
+echo "******************************* Update and install grafana *******************************"
 sudo apt-get update -y
 sudo apt-get install grafana
 
-echo "Starting server"
+echo "******************************* Starting server *******************************"
 sudo /bin/systemctl start grafana-server
